39387b11a7ae222a636d5dda9b6eb9c0514c1d82
Bilostenko <bilostenkod@gmail.com>
2023-08-11 23:39:25 +0300

    add layout and partly logic for temperature conv

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,14 @@ import './index.css';
 
 import Counter from './counter/counter';
 import TaskList from './todo-list/task-list';
+import TemperatureConverter from './temperature converter/temperatureConverter';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <Counter  counter={10}/>
     <TaskList />
+    <TemperatureConverter />
   </React.StrictMode>
 );
 
--- src/temperature converter/temperatureConverter.jsx
+++ src/temperature converter/temperatureConverter.jsx
@@ -12,3 +12,63 @@
 // const fahrenheitValue = (celsiusValue * 9 / 5) + 32;
 // const celsiusValue = (fahrenheitValue - 32) * 5 / 9;
 // івент.таргет.валуе
+
+import React from "react";
+import { Component } from "react";
+import "./temperatureConverter.css"
+
+class TemperatureConverter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      celsius: 0,
+      fahrenheit: 0
+    }
+    this.newFahrenheitValue = React.createRef()
+  }
+
+  handleFahrenheitChange = () => {
+    let currentFahrenheitValue = this.newFahrenheitValue.current.value;
+    let celsiusValue = ((currentFahrenheitValue - 32) * 5 / 9).toFixed(2);
+
+      this.setState({
+        celsius: celsiusValue
+      })
+
+  }
+
+  render() {
+    return (
+      <div className="converter-container">
+        <h2 className="converter-title">Конвертер градусів</h2>
+        <div className="input-container">
+          <input
+            type="number"
+            value={this.state.celsius}
+            ref={this.newCelsiusValue}
+            onChange={this.handleCelsiusChange}
+            placeholder="Градуси Цельсія"
+            className="input-celsius" />
+
+          <div className="arrows">
+            <div className="arrow-icon">&#129154;</div>
+            <div className="arrow-icon">&#129152;</div>
+          </div>
+
+          <input
+            type="number"
+            // value={this.fahrenheit}
+            ref={this.newFahrenheitValue}
+            onChange={this.handleFahrenheitChange}
+            placeholder="Градуси Фаренгейта"
+            className="input-fahrenheit" />
+        </div>
+      </div>
+
+
+
+    )
+  }
+}
+
+export default TemperatureConverter
