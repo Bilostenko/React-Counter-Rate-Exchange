109a5f715d45e8ffa5e3816d7de9eaa0126396a6
Bilostenko <bilostenkod@gmail.com>
2023-10-10 15:18:45 +0300

    add project

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import Counter from './counter/counter';
-import TaskList from './todo-list/task-list';
-import TemperatureConverter from './temperature converter/temperatureConverter';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <Counter  counter={10}/>
-    <TaskList />
-    <TemperatureConverter />
   </React.StrictMode>
 );
 
